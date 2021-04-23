2370ac21f6c0d373d77abea93f9d02a2694f9e07
nofar carmel <nofarcar@bgu.ac.il>
2021-04-23 13:28:51 +0300

    config file

---
+++ src/Config/config.js
@@ -0,0 +1,10 @@
+let token;
+
+export function setToken(newToken) {
+    token = newToken;
+}
+
+export function TOKEN() {
+  return token;  
+} 
+export const API_URL = 'http://ec2-3-16-130-74.us-east-2.compute.amazonaws.com:8080'
