587ceaf2bf09e0c655a1e7f48ceee0e789e15247
NoyEzra <noyez@post.bgu.ac.il>
2021-05-01 19:58:10 +0300

    Restrictions page - managed to update restriction on page load. problem with empty restriction at start

--- src/components/Restrictions/Restrictions.js
+++ src/components/Restrictions/Restrictions.js
@@ -1,7 +1,9 @@
-import React, { useState, Text } from 'react';
+import React, { useState, Text, useEffect } from 'react';
 import classes from './Restrictions.module.css'
 import { FormControlLabel, Radio, RadioGroup, TextField } from '@material-ui/core';
 import DaysCheckBox from '../DaysCheckBox/DaysCheckBox';
+import Axios from 'axios';
+import { API_URL, TOKEN } from '../../Config/config';
 
 const employees = [
     {
@@ -50,6 +52,42 @@ const employees = [
 function Restrictions() {
     const [empRestrictions, updateRestrictions] = useState(employees)
     const [employee, selectEmployee] = useState(empRestrictions[0]);
+
+
+
+
+    const addEmpRestriction = (employeeName, allowedDays) => {
+        console.log(employeeName)
+        console.log(allowedDays)
+        var allowed = [false, false, false, false, false]
+        for(var i = 0; i < allowedDays.length; i++){
+            var day = allowedDays[i]
+            if(day >= 0 && day < 5){
+                allowed[day] = true 
+            }
+        }
+        console.log(allowed)
+
+        updateRestrictions(empRestrictions.push({name: employeeName, restrictions: allowed}))
+    } 
+
+    //triggered onpage load
+    useEffect(() => {
+        Axios.get(`${API_URL}/managers/getEmployeesRestrictions`, {headers: {
+            'Authorization': `Bearer ${TOKEN()}`,
+          }}).then(({data}) => {
+              console.log(data)
+            
+            for (const [key, value] of Object.entries(data)) {
+                addEmpRestriction(key, value['_allowed_days'])
+            } 
+            console.log(empRestrictions)
+            if(empRestrictions.lengh > 0){
+                selectEmployee(empRestrictions[0])
+            }
+        }).catch((err)=> console.log(err))
+    }, []);
+
 
     const updateUserRestrictions = (restrictions) => {
         const newEmpRestriction = empRestrictions.map((emp) => emp.name !== employee.name ? emp : { name: emp.name, restrictions });
