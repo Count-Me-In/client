3f1a427f2f5ef400e84cd0144cef0d4a62c65f00
nofar carmel <nofarcar@bgu.ac.il>
2021-05-31 20:02:13 +0300

    cypress- login page tests

---
+++ tests/cypress/integration/examples/CMITests/LoginPageTests/LoginPageFunctionality.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+context('Log in page functionality tests', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/login')
+    })
+  
+    it('We Can log in with correct user info', () => {
+      cy.get('[data-testid=nameField]').type('admin')
+      cy.get('[data-testid=passwordField]').type('password')
+      cy.get('[data-testid=loginBtn]').click()
+      cy.url().should('include', '/home') 
+    })
+
+    it('We cant login without all the user info', () => {
+      cy.get('[data-testid=loginBtn]').click()
+      cy.url().should('include', '/login')
+      cy.get('[data-testid=loginAlert]').should('exist')
+
+      cy.get('[data-testid=nameField]').type('admin')
+      cy.get('[data-testid=loginBtn]').click()
+      cy.get('[data-testid=loginAlert]').should('exist')
+
+      cy.get('[data-testid=nameField]').clear()   
+      cy.get('[data-testid=passwordField]').type('admin')
+      cy.get('[data-testid=loginBtn]').click()
+      cy.get('[data-testid=loginAlert]').should('exist')
+    })
+
+    it('We can press the INFO button', () => {
+      cy.get('[data-testid=InfoBtn]').click()
+      cy.get('[data-testid=infoBtnText]').should('exist')
+    })
+  })
